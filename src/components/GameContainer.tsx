c07b0a0f9bff4fc2904c9d8572ac4559565dfc92
jeraruk <107743662+jeraruk@users.noreply.github.com>
2022-11-07 17:35:38 +1100

    Adding guess options and score

--- src/components/GameContainer.tsx
+++ src/components/GameContainer.tsx
@@ -1,40 +1,116 @@
-import React, {useState, useEffect} from 'react'
-import { fetchPokemonApi, fetchSinglePokemonApi } from '../api/pokemonApi'
-
+import React, { useState, useEffect } from "react";
+import { fetchPokemonApi, fetchSinglePokemonApi } from "../api/pokemonApi";
 
 const GameContainer = () => {
-const [pokemonList, setPokemonList] = useState([])
-const [correctPokemon, setCorrectPokemon] = useState({img: "", name: ""})
+  const [pokemonList, setPokemonList] = useState([{ name: "" }]);
+  const [correctPokemon, setCorrectPokemon] = useState({ img: "", name: "" });
+  const [score, setScore] = useState(0);
+  const [guessOptions, setGuessOptions] = useState([]);
+  const [timer, setTimer] = useState(30);
+  const [isGameOver, setIsGameOver] = useState(false);
 
-useEffect(() => {
+  useEffect(() => {
     fetchPokemonApi().then((data: any) => {
-        setPokemonList(data)
-        console.log("yo", data)
-    })
+      setPokemonList(data);
+      console.log("yo", data);
+    });
+  }, []);
 
-    startRound()
-}, [])
-    
+  useEffect(() => {
+    console.log(timer);
+    if (timer <= 0) {
+      clearInterval(timerInterval);
+      setIsGameOver(true);
+    }
+  }, [timer]);
 
-    async function startRound () {
-        let randomCorrectPokemon : number = Math.floor((Math.random() * 150) + 1 )
-        let data = await fetchSinglePokemonApi(randomCorrectPokemon)
+  function startTimerAndRound() {
+    setScore(0);
+    setTimer(30);
+    clearInterval(setInterval(reduceTimer, 1000));
+    startRound();
+    console.log(2);
 
-        setCorrectPokemon(data)
-        console.log(data)
+    let timerInterval = setInterval(reduceTimer, 1000);
+    console.log(5);
+
+    console.log(3);
+  }
+
+  function reduceTimer() {
+    console.log(timer);
+    if (timer == 0) {
+      clearInterval(setInterval(reduceTimer, 1000));
+      setIsGameOver(true);
+    } else {
+      setTimer((prev) => prev - 1);
+    }
+  }
+
+  async function startRound() {
+    let randomCorrectPokemon: number = Math.floor(Math.random() * 150 + 1);
+    let data = await fetchSinglePokemonApi(randomCorrectPokemon);
+    let tempGuessOptions = [];
+    setCorrectPokemon(data);
+
+    tempGuessOptions.push(data.name);
+
+    while (tempGuessOptions.length < 4) {
+      let random: number = getRandomPokemonNumber();
+      console.log(random);
+      if (
+        pokemonList[random].name !== data.name &&
+        !tempGuessOptions.includes(pokemonList[random].name)
+      ) {
+        tempGuessOptions.push(pokemonList[random].name);
+      }
     }
 
+    setGuessOptions(
+      tempGuessOptions.sort(() => (Math.random() > 0.5 ? 1 : -1))
+    );
+    console.log(tempGuessOptions);
+  }
+
+  function getRandomPokemonNumber(): number {
+    return Math.floor(Math.random() * 150 + 1);
+  }
+
+  const checkGuess = (pokemon: String) => {
+    if (correctPokemon.name === pokemon) {
+      setScore((prev) => prev + 1);
+      startRound();
+    } else {
+      setIsGameOver(true);
+    }
+  };
 
   return (
     <div>
-        <button onClick={() =>startRound()}>Start</button>
-        <div>
-            <img src={correctPokemon.img} alt={correctPokemon.name} />
-        </div>
-        <div></div>
-
+      <div>Score: {score}</div>
+      <div>Timer: {timer}</div>
+      <div>{isGameOver ? "GameOver" : null}</div>
+      <button onClick={() => startTimerAndRound()}>Start</button>
+      <div>
+        <img src={correctPokemon.img} alt={correctPokemon.name} />
+      </div>
+      <div>
+        {guessOptions?.map((item) => {
+          return (
+            <button
+              disabled={isGameOver}
+              onClick={() => {
+                checkGuess(item);
+              }}
+              key={item}
+            >
+              {item}
+            </button>
+          );
+        })}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default GameContainer
+export default GameContainer;
