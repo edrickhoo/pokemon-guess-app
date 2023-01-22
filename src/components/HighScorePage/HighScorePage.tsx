6ea95449b993616dfd0391ce78c846170e25f8f3
jeraruk <107743662+jeraruk@users.noreply.github.com>
2023-01-23 00:18:35 +1100

    Changed modal bg to black

--- src/components/HighScorePage/HighScorePage.tsx
+++ src/components/HighScorePage/HighScorePage.tsx
@@ -9,15 +9,21 @@ const HighScorePage = () => {
   }, []);
 
   return (
-    <div className="max-w-[1440px] mx-auto py-12 px-8">
-      {highScores
-        ? highScores.map((score: any, idx: number) => (
-            <div key={idx} className="flex items-center">
-              <div className="flex-[4]">{score.name}</div>
-              <div className="flex-1 text-center">{score.score}</div>
-            </div>
-          ))
-        : null}
+    <div className="max-w-[400px] w-full mx-auto py-12 px-8">
+      <div className="flex items-center border-b">
+        <div className="flex-[4]">Name</div>
+        <div className="flex-1 text-center">Score</div>
+      </div>
+      <div className="space-y-2">
+        {highScores
+          ? highScores.map((score: any, idx: number) => (
+              <div key={idx} className="flex items-center">
+                <div className="flex-[4]">{score.name}</div>
+                <div className="flex-1 text-center">{score.score}</div>
+              </div>
+            ))
+          : null}
+      </div>
     </div>
   );
 };
