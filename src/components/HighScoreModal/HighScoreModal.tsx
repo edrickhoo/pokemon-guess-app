4d496d73edcf942d367bab6063adf782c7a04721
jeraruk <107743662+jeraruk@users.noreply.github.com>
2023-01-25 10:25:39 +1100

    Added interfaces for typing

--- src/components/HighScoreModal/HighScoreModal.tsx
+++ src/components/HighScoreModal/HighScoreModal.tsx
@@ -1,17 +1,27 @@
+import { highScoreData } from "../../api/highScoresApi";
+
+interface props {
+  score: number;
+  highScores: highScoreData[];
+  submitScore: () => void;
+  setNameInput: React.Dispatch<React.SetStateAction<any>>;
+  setHighScoreModal: React.Dispatch<React.SetStateAction<any>>;
+}
+
 const HighScoreModal = ({
   score,
   highScores,
   submitScore,
   setNameInput,
   setHighScoreModal,
-}: any) => {
+}: props) => {
   return (
     <div>
       <div
         onClick={() => setHighScoreModal((prev: boolean) => !prev)}
         className="fixed top-0 z-10 left-0 h-[100vh] w-full bg-black/40"
       ></div>
-      <div className="fixed top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] z-20 bg-gray-300 py-8 px-8 rounded-md space-y-3">
+      <div className="fixed top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] z-20 bg-black py-8 px-8 rounded-md space-y-3">
         <div>
           <h3 className="text-center">High Scores</h3>
           <div className="flex items-center">
@@ -19,7 +29,7 @@ const HighScoreModal = ({
             <div className="flex-1">Score</div>
           </div>
           {highScores
-            ? highScores.map((score: any, idx: number) => {
+            ? highScores.map((score: highScoreData, idx: number) => {
                 return (
                   <div key={idx} className="flex items-center">
                     <div className="flex-[4]">{score.name}</div>
